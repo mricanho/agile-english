df2dddb4925a42ab21b9bce56c37f3376f23b7b4
Miguel Ricaño <mricanho@gmail.com>
2021-07-25 17:58:52 -0500

    Add endpoints to index.js Axion

---
+++ src/api/endpoints.js
@@ -0,0 +1,6 @@
+export const LOGIN = () => ({ method: 'POST', url: '/login' });
+export const REGISTER = () => ({ method: 'POST', url: '/register' });
+export const GET_LECTURES = () => ({ method: 'GET', url: '/lectures' });
+export const GET_APPOINTMENTS = () => ({ method: 'GET', url: '/appointments' });
+export const POST_APPOINTMENT = () => ({ method: 'POST', url: '/appointments' });
+export const DELETE_APPOINTMENT = () => ({ method: 'DELETE', url: '/appointments' });
