38a93863fc8ce096b6dafb5d3dc4b0dbe020535d
Miguel Ricaño <mricanho@gmail.com>
2021-07-26 18:35:45 -0500

    Add combine reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,20 @@
+import { combineReducers } from 'redux';
+import adminReducer from './admin';
+import uiSnackbarReducer from './uiSnackbar';
+import userReducer from './user';
+import uiModalReducer from './uiModal';
+import lectureReducer from './lecture';
+import uiDrawerReducer from './uiDrawer';
+import appointmentsReducer from './appointments';
+
+const rootStore = combineReducers({
+  admin: adminReducer,
+  lectures: lectureReducer,
+  uiSnackBar: uiSnackbarReducer,
+  uiModal: uiModalReducer,
+  uiDrawer: uiDrawerReducer,
+  user: userReducer,
+  appointments: appointmentsReducer,
+});
+
+export default rootStore;
